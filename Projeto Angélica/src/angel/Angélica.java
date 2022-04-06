c266e7c15bfb91d9248ae4fb31396f683f064aca
angelicapessoa <102373004+angelicapessoa@users.noreply.github.com>
2022-04-06 15:43:09 -0300

    Add files via upload

---
+++ Projeto Angélica/src/angel/Angélica.java
@@ -0,0 +1,19 @@
+package angel;
+
+public class Angélica {
+
+	public static void main(String[] args) {
+		String nome = "Angélica";
+		int idade = 28;
+		char sexo = 'M';
+		double temperatura = 26.7;
+		boolean arcondicionado = false;
+		System.out.println("Nome: "+ nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Sexo: " + sexo);
+		System.out.println("Temperatura: " + temperatura);
+		System.out.println("Ar condiconado: " + arcondicionado);
+
+	}
+
+}
